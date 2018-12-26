d7d0c2b9523688b29967ff31adc0bad5c0b0e9fd
Renato Xavier <you@example.com>
2018-12-26 09:32:31 -0200

    ngx-bootstrap buttons and tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonsModule } from 'ngx-bootstrap';
+import { TooltipModule } from 'ngx-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonsModule.forRoot(),
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
